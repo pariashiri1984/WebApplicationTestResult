2055a2f52b10f997f119d19846480e9c0cdda877
Paria Shiri <paria.shiri20@gmail.com>
2021-07-01 15:05:41 +0100

    clean the project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApplicationTestResult.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+     
+
+        public HomeController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+          
+        }
+    }
+}

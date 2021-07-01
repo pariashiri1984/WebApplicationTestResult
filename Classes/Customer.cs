e92ae67fb0eca08f1fe991009a6622f3a08c3822
Paria Shiri <paria.shiri20@gmail.com>
2021-07-01 15:09:55 +0100

    Customer class done

---
+++ Classes/Customer.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationCustomerInvitaion.Classes
+{/// <summary>
+/// Map text data to customer object
+/// </summary>
+    public class Customer
+    {
+        [JsonProperty("latitude")]
+        private string stringLatitude { get; set; }
+
+        [JsonProperty("user_id")]
+        public int UserId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("longitude")]
+        private string stringLongitude { get; set; }
+
+        //convert string to double
+        [NotMapped]
+        public double Latitude { 
+            get { return Double.Parse(stringLatitude); }
+        }
+        //convert string to double
+        [NotMapped]
+        public double Longitude
+        { 
+            get { return Double.Parse(stringLongitude); }
+        }
+
+    }
+}
